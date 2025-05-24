173842e6f59b3aa85e5c0a7df131534714af4873
Olof Johansson <olof@liberdev.se>
2025-05-24 13:53:18 +0200

    Add a controller for registering new users

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Entity\Admin;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class RegistrationController extends AbstractController
+{
+
+    protected EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;        
+    }
+
+    #[Route('/register', name: 'register')]
+    public function index(UserPasswordHasherInterface $passwordHasher, ValidatorInterface $validator, EntityManagerInterface $entity_manager, Request $request): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', TextType::class)
+            ->add('password', PasswordType::class)
+            ->add('save', SubmitType::class, ['label' => 'Register'])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $admin = new Admin();
+            $roles = $admin->getRoles();
+            // $roles[] = "ROLE_SUPER_ADMIN";
+
+            $formData = $form->getData();
+            $email = $formData["email"];
+
+            $plaintextPassword = $formData["password"];
+
+            $admin->validateRawPassword($plaintextPassword);
+
+            $hashedPassword = $passwordHasher->hashPassword(
+                $admin,
+                $plaintextPassword
+            );
+
+            $admin->setPassword($hashedPassword);
+            $admin->setRoles($roles);
+            $admin->setEmail($email);
+            // $admin->setIsActive(false);
+
+            $errors = $validator->validate($admin);
+
+            if (count($errors) < 1) {
+                $this->entityManager->persist($admin);
+                $this->entityManager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'User created.'
+                );
+                return $this->redirectToRoute('badger_show_all');
+            }
+        }
+
+        return $this->render('register/index.html.twig', [
+            'title' => "Register a new user",
+            'form' => $form,
+            'errors' => $errors ?? null
+        ]);
+    }
+}
