8d45935b9a85e13c511061a9f2cffc3ff34c0bbd
Olof Johansson <olof@liberdev.se>
2025-05-24 20:27:19 +0200

    Add functionality for editing and deleting badgers

--- src/Controller/BadgerController.php
+++ src/Controller/BadgerController.php
@@ -4,9 +4,13 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Entity\Badger;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class BadgerController extends AbstractController
 {
@@ -41,6 +45,99 @@ class BadgerController extends AbstractController
 
         return $this->render("badger/list.html.twig", [
             "badgers" => $badgers
+        ]);
+    }
+
+    #[Route('/edit/badger/{id}', name: 'badger_edit')]
+    public function edit(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $badger = $entityManager->getRepository(Badger::class)->find($id);
+
+        if (!$badger) {
+            throw $this->createNotFoundException(
+                'No badger found for id ' . $id
+            );
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class, [ 'data' => $badger->getName() ])
+            ->add('continent', TextType::class, [ 'data' => $badger->getContinent() ])
+            ->add('description', TextType::class, [ 'data' => $badger->getDescription() ])
+            ->add('save', SubmitType::class, ['label' => 'Save Badger'])
+            ->getForm();
+
+
+        $form->handleRequest($request);
+        $formData = $form->getData();
+
+        if ($form->isSubmitted()) {
+            $badger->setName($formData["name"]);
+            $badger->setContinent($formData["continent"]);
+            $badger->setDescription($formData["description"]);
+
+            $errors = $validator->validate($badger);
+
+            if (count($errors) < 1) {
+                $entityManager->persist($badger);
+                $entityManager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Your changes were saved!'
+                );
+                return $this->redirectToRoute('badger_edit', ['id' => $id]);
+            }
+        }
+
+        return $this->render("badger/edit.html.twig", [
+            "form" => $form
+        ]);
+    }
+
+
+    #[Route('/delete/badger/{id}', name: 'badger_delete')]
+    public function delete(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager, int $id): Response
+    {
+        $badger = $entityManager->getRepository(Badger::class)->find($id);
+
+        if (!$badger) {
+            throw $this->createNotFoundException(
+                'No badger found for id ' . $id
+            );
+        }
+
+        $form = $this->createFormBuilder()
+            ->add('name', TextType::class, [ 'data' => $badger->getName(),
+            'attr' => [ 'disabled' => true ] ])
+            ->add('continent', TextType::class, [ 'data' => $badger->getContinent(),
+            'attr' => [ 'disabled' => true ] ])
+            ->add('description', TextType::class, [ 'data' => $badger->getDescription(),
+            'attr' => [ 'disabled' => true ] ])
+            ->add('save', SubmitType::class,
+                [ 'label' => 'I understand, delete this badger',
+                'attr' => [ 'class' => 'btn-danger' ]])
+            ->getForm();
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $errors = $validator->validate($badger);
+
+            if (count($errors) < 1) {
+                $entityManager->remove($badger);
+                $entityManager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Item deleted!'
+                );
+                return $this->redirectToRoute('app_home');
+            }
+        }
+
+        return $this->render("badger/delete.html.twig", [
+            "form" => $form
         ]);
     }
 }
