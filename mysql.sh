0597999ca9a96632faf5161c2d7815b413fa9e1c
Olof Johansson <olof@liberdev.se>
2025-05-25 23:04:38 +0200

    Fix CI

--- mysql.sh
+++ mysql.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-docker exec -it symfony-docker-database-1 bash -c "mysql app -uapp -p\!ChangeMe\!"
+docker exec -it symfony-docker-database-1 bash -c "mysql app -uapp -pbLas_regn42_"
 
