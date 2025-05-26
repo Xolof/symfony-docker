5ebc5cc42b17d52d70c1ba9d50dd357b542213fc
Olof Johansson <olof@liberdev.se>
2025-05-26 22:44:35 +0200

    Add creation of fake badgers and users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,21 +2,42 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Admin;
 use App\Entity\Badger;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory as FakerFactory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $faker = FakerFactory::create();
+
+        echo "Creating fake Badgers...\n";
+
         for ($i = 0; $i < 100; $i++) {
             $badger = new Badger;
-            $badger->setName("Badger $i");
-            $badger->setContinent("Continent $i");
-            $badger->setDescription("The American Badger is a fascinating and often misunderstood creature. Here's a detailed breakdown of its characteristics, behavior, habitat, and conservation status: 1. Physical Characteristics: Size: Relatively stout and low to the ground. Length ranges from 23-35 inches (58-89 cm), including the tail. Weighs between 9-26 pounds (4-12 kg), with males being larger. Appearance: Recognizable by its distinctive markings. Color: Generally grayish, with a black face marked with a white stripe running from the nose to the eyes. Often has white markings on its crown. Body: Powerful, muscular build with short legs and long claws on the front feet. These claws are essential for digging. Tail: Short and bushy. Head: Broad head with small ears. Distinctive Feature: The striking facial markings are key to identification. The white stripe is almost like a mask.");
+            $badger->setName($faker->name());
+            $badger->setContinent($faker->name());
+            $badger->setDescription($faker->realText(1200));
 
             $manager->persist($badger);
+            $manager->flush();
+        }
+
+        echo "Creating fake Users...\n";
+
+        for ($i = 0; $i < 100; $i++) {
+
+            $password = implode('', $faker->randomElements(range('A', 'z'), 20));
+
+            $user = new Admin;
+
+            $user->setPassword($password);
+            $user->setEmail($faker->email());
+
+            $manager->persist($user);
             $manager->flush();
         }
     }
