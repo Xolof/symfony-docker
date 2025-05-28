2ae43ac2ac819c9780e9f5b9cc0ee4e7bb62c1f9
Olof Johansson <olof@liberdev.se>
2025-05-28 13:45:09 +0000

    Customize the script for starting a bash shell within the docker php container

--- docker.sh
+++ docker.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
-docker exec -it symfony-docker-php-1 bash
 
+email="$(git config --global user.email)"
+name="$(git config --global user.name)"
+
+docker compose exec -T php git config --global --add safe.directory /app
+
+docker exec -it -e  XDEBUG_MODE=coverage \
+    symfony-docker-php-1 bash -c \
+    "echo \"alias st='git status'\" >> ~/.bashrc; source ~/.bashrc; \
+     git config --global --replace-all user.email \"$email\"; \
+     git config --global --replace-all user.name \"$name\"; \
+     bash"
