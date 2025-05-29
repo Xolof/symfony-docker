6079903df5df675bfb4f83b3d2c0f5549f302d37
Olof Johansson <olof@liberdev.se>
2025-05-29 19:36:50 +0200

    Update tests script

--- tests.sh
+++ tests.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
-./vendor/bin/phpcs src tests
-./vendor/bin/phpcbf src tests
-# ./vendor/bin/pint src templates config tests
+./vendor/bin/phpcbf src tests --standard=phpcs.xml
+./vendor/bin/phpcbf src tests --standard=phpcs_psr12.xml
+
+./vendor/bin/phpcs src tests --standard=phpcs.xml
+./vendor/bin/phpcs src tests --standard=phpcs_psr12.xml
+
 ./vendor/bin/rector src templates config tests
-./vendor/bin/phpstan analyse src --memory-limit=1G
+
+./vendor/bin/phpstan analyse src --level=6 --memory-limit=1G
 
 export XDEBUG_MODE=coverage && php bin/phpunit --coverage-html coverage
-
